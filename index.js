a59e997eb1cf43c2d9c18da5396efcfc0582ac7a
kimditto <69024694+kimditto@users.noreply.github.com>
2020-07-31 14:45:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Nick & Sam!');
+    res.send('I like all!');
 });
 
 app.get('/start', function(req, res){
